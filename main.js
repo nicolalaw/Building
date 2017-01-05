34b0d5ea099d67c634cd6c9a22c157dcf5277850
YuenLam Law <yll2110@Jins-iMac.local>
2017-01-05 18:49:21 -0500

    added cubes and javascript to turn cubes

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// testesttest
+var cubeTurned = false;
+
+function turn(right) {
+  if (right) {
+    document.querySelector('.j-cube').classList.add('j-turnRight');
+  } else {
+    document.querySelector('.j-cube').classList.remove('j-turnRight');
+  }
+  cubeTurned = !cubeTurned;
+}
+
+window.setInterval(function(){
+  if (cubeTurned) {
+    turn(false);
+  } else {
+    turn(true);
+  }
+}, 1500);
